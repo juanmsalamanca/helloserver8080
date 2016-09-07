2612964b3cc2ff8109e039ba0ce2db14faed77f4
juan salamanca <juan salamanca>
2016-09-06 19:50:03 -0500

    first

---
+++ server.js
@@ -0,0 +1,12 @@
+/**
+ * Created by juan on 06/09/2016.
+ */
+var http = require('http');
+var server = http.createServer();
+function control(petic, resp) {
+    resp.writeHead(200, {'content-type': 'text/plain'});
+    resp.write('Hola, Mundo!');
+    resp.end();
+}
+server.on('request', control);
+server.listen(8080);
