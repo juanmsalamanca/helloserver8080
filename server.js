f222f787779805c071f63ecab4d90a0cf56fb831
juan salamanca <juan salamanca>
2016-09-06 19:57:28 -0500

    2

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
  */
 var http = require('http');
 var server = http.createServer();
+console.log("running");
 function control(petic, resp) {
     resp.writeHead(200, {'content-type': 'text/plain'});
     resp.write('Hola, Mundo!');
     resp.end();
+
 }
 server.on('request', control);
 server.listen(8080);
